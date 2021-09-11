83491e192d8b6a6f355c6cc208c73fcd43f84c42
Sam Warnick <sam.t.warnick@gmail.com>
2021-09-11 13:02:15 -0400

    Initial Commit

---
+++ Road Noise/Road_NoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Road_NoiseApp.swift
+//  Road Noise
+//
+//  Created by Sam Warnick on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Road_NoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
